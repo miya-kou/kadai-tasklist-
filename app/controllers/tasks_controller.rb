1d02784d141315a65b1c4ff40bb3a24554fe8de5
kohei.miyamoto <ponpokopomu@gmail.com>
2020-05-25 14:06:57 +0000

    revise

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,15 +4,15 @@ class TasksController < ApplicationController
   end
 
   def show
-     @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-     @task = Task.new
+    @task = Task.new
   end
 
   def create
-     @task = Task.new(task_params)
+    @task = Task.new(task_params)
 
     if @task.save
       flash[:success] = 'タスクが投稿されました'
@@ -28,28 +28,28 @@ class TasksController < ApplicationController
   end
 
   def update
-     @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
 
-  if @task.update(task_params)
-    flash[:success] = 'タスクが編集されました'
-    redirect_to @task
-  else
-    flash.now[:danger] = 'タスクが編集されませんでした'
-    render :new
-  end
+    if @task.update(task_params)
+      flash[:success] = 'タスクが編集されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'タスクが編集されませんでした'
+      ender :new
+    end
   end
 
   def destroy
-  @task = Task.find(params[:id])
-  @task.destroy
+    @task = Task.find(params[:id])
+    @task.destroy
 
-  flash[:success] = 'タスクが削除されました'
-  redirect_to tasks_path
+    flash[:success] = 'タスクが削除されました'
+    redirect_to tasks_path
   end
-end
+  
+  private
 
-private
-
-def task_params
-  params.require(:task).permit(:content)
+　def task_params
+    params.require(:task).permit(:content)
+　end
 end
