7159bf4a43b410a8795a9d314714872e0b8a1b2f
kohei.miyamoto <ponpokopomu@gmail.com>
2020-05-27 08:49:42 +0000

    amendment_3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    set_message
+    set_task
   end
 
   def new
@@ -25,7 +25,7 @@ class TasksController < ApplicationController
   end
 
   def edit
-    set_message
+    set_task
   end
 
   def update
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    set_message
+    set_task
     @task.destroy
 
     flash[:success] = 'タスクが削除されました'
